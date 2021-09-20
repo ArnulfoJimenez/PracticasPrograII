7cab5d7691d703685021904069b21f137478dc92
ArnulfoJimenez <87824514+ArnulfoJimenez@users.noreply.github.com>
2021-09-20 16:23:59 -0600

    Practica

---
+++ PracticaPresencial2/PracticaPresencial2/Controllers/UsuarioController.cs
@@ -0,0 +1,43 @@
+﻿using PracticaPresencial2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PracticaPresencial2.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        // GET: Usuario
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        
+        public ActionResult Index(Usuario usuario)
+        {
+            if (ModelState.IsValid)
+            {
+                return View("Registro");
+            }
+            else
+            {
+                return View("Index", usuario);
+                ////return View("Error", usuario);
+            }
+
+        }
+        
+
+        public ActionResult Registro()
+        {
+            return View();
+        }
+        //public ActionResult Error()
+        //{
+        //    return View();
+        //}
+    }
+}
---
+++ PracticaPresencial2/PracticaPresencial2/Models/Usuario.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace PracticaPresencial2.Models
+{
+    public class Usuario
+    {
+   [Display (Name = "Nombre")]
+   [Required(ErrorMessage = "Error este campo es requirido")]
+        public String Nombre { get; set; }
+
+   [Display(Name = "Edad")]
+   [Required(ErrorMessage = "Error este campo es requirido")]
+   public int Edad { get; set; }
+
+    [Display(Name = "Sexo")]
+    [Required(ErrorMessage = "Error este campo es requirido")]
+    public String Sexo { get; set; }
+
+    [Display(Name = "Tipo De estudio")]
+    [Required(ErrorMessage = "Error este campo es requirido")]
+     public String TipoDeEstudio { get; set; }
+    }
+}
