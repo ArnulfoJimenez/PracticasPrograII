6f3b7f0ce9bfbfba32609b15a3a3b1faafb7d588
ArnulfoJimenez <87824514+ArnulfoJimenez@users.noreply.github.com>
2021-09-23 13:24:38 -0600

    NetCore

---
+++ Practica.NetCore/Practica.NetCore/Controllers/UsuarioController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Practica.NetCore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Practica.NetCore.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        public IActionResult Welcome()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Welcome(Usuarios usuario)
+        {
+            if (ModelState.IsValid)
+            {
+                return View("Registro");
+            }
+            else
+            {
+                return View("Welcome", usuario);
+            }
+
+        }
+        public IActionResult Registro() 
+        {
+            return View();
+        }
+    }
+}
---
+++ Practica.NetCore/Practica.NetCore/Models/Usuarios.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Practica.NetCore.Models
+{
+    public class Usuarios
+    {
+        [Display (Name ="Nombre")]
+        [Required (ErrorMessage = "Este campo es requerido")]
+        public String Nombre { get; set; }
+
+        [Display(Name = "Edad")]
+        [Required(ErrorMessage = "Este campo es requerido")]
+       
+        public int Edad { get; set; }
+
+        [Display(Name = "Sexo")]
+        [Required(ErrorMessage = "Este campo es requerido")]
+        public String Sexo { get; set; }
+
+        [Display(Name = "Tipo De Estudio")]
+        [Required(ErrorMessage = "Este campo es requerido")]
+        public String TipoDeEstudio {get; set; }
+    }
+}
